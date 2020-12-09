6bf8909ccb6d94980eea1fa66662d4b8c50dc5a9
Tom_lim <j17025089@student.newinti.edu.my>
2020-12-09 19:19:15 +0800

    updated menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,11 @@ class Directory extends React.Component {
     return(
       <div className='directory-menu'>
         { 
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ id, ...otherSectionsProps }) => (
+            <MenuItem 
+              key={id} 
+              {...otherSectionsProps}
+            />
           )) 
         }
       </div>
