3fe1cc082d6c742db012fa6321a9f8270214a05c
Tom_lim <j17025089@student.newinti.edu.my>
2020-12-11 01:57:52 +0800

    added sign-in page, form-input component and custom-button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
